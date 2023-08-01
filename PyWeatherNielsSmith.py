f74e1a113127594c3538e00011b8f028ca7d77f2
Niels Smith <30351969+smithniels@users.noreply.github.com>
2023-08-01 11:39:28 +0200

    pushy

--- PyWeatherNielsSmith.py
+++ PyWeatherNielsSmith.py
@@ -2,11 +2,11 @@
 
 import requests
 
-CITY_NAME = "Concord, New hampshire"
+CITY_NAME = "Copenhagen"
 API_KEY = "290a1bec5e623e9a6791b687e8f47bd3"
 
 url = f"https://api.openweathermap.org/data/2.5/weather?q={CITY_NAME}&appid={API_KEY}"
-response = requests.get(url)
+response = requests.get(url, timeout=5)
 
 if response.status_code == 200:
     weather_data = response.json()
@@ -15,10 +15,8 @@ if response.status_code == 200:
     temperature_celsius = temperature_kelvin - 273.15
     temperature_celsius = round(temperature_celsius, 2)
     temperature_fahrenheit = round((temperature_celsius*9/5)+32, 2)
-    # humidity = weather_data["main"]["humidity"]
     print(f"Temperature: {temperature_celsius} Celsius")
-    print(f"Temperature: {temperature_fahrenheit} Farhrenheit")
+    print(f"Temperature: {temperature_fahrenheit} Fahrenheit")
     print(f"Weather: {weather_description}")
-    # print(f"Humidity: {humidity}")
 else:
     print("No weather today. Sucks to suck, bummer")
